dfe476bd835d5b3b3f212e4f113ca457ad2a8d8f
Esteve Soria <esofabian@gmail.com>
2016-02-22 23:29:37 +0100

    App ready to translations

--- Parlepertu.pro
+++ Parlepertu.pro
@@ -2,9 +2,17 @@ TEMPLATE = app
 
 QT += qml quick widgets
 
+#Allows to get the strings to translate in QML files
+lupdate_only {
+	SOURCES = *.qml \
+}
+
 SOURCES += main.cpp
 
 RESOURCES += qml.qrc
+
+TRANSLATIONS = Parlepertu_es.ts \
+				Parlepertu_en.ts
 
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH =
---
+++ Parlepertu_es.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>MainForm</name>
+    <message>
+        <source>Press Me 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Press Me 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Press Me 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>May I have your attention, please?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
